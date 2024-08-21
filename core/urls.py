007ee2aa3ca8243a3e9d0058763b7c5b891002cd
Glen Hayoge <glenhayoge@gmail.com>
2024-08-21 12:06:03 +1000

    configured frontend components

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from identifier.views import PlantViewSet 
-
-router = routers.DefaultRouter()
-router.register(r'plants', PlantViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('', include('identifier.urls')),
 ]
--- identifier/urls.py
+++ identifier/urls.py
@@ -1,12 +1,10 @@
-from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 from .views import PlantViewSet 
 
-router = routers.DefaultRouter()
-router.register(r'plants', PlantViewSet)
+router = DefaultRouter()
+router.register(r'plants', PlantViewSet )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
